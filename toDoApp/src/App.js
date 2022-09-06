afc7c0390e6ca027a481fc3723f629a39842aaab
Zeynep Beyza <zynpbyza0@gmail.com>
2022-09-06 13:26:17 +0300

    todos_work

---
+++ toDoApp/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+import ToDoApp from './Components/ToDoApp/ToDoApp';
+import Footer from './Components/Footer/Footer';
+
+
+function App() {
+  return (
+    <div className="App">
+
+      <ToDoApp />
+
+      <Footer/>
+      
+    </div>
+  );
+}
+
+export default App;
